831174bba64e5043cfb9ff78b9e4ce6d962bba91
cjhsu1991 <dd86042@gmail.com>
2021-07-15 20:39:54 +0800

    Update beatuifulsoup.py

--- beatuifulsoup.py
+++ beatuifulsoup.py
@@ -5,14 +5,15 @@
 # 使用 beautifulsoup 分析爬取的資料
 import requests
 from bs4 import BeautifulSoup
-url = 'https://invest.cnyes.com/twstock/TWS/2603/overview'
+url = '要爬取的url'
 response = requests.get(url)
-# tv-lightweight-charts
+
+# 如果只有一個的話
 soup = BeautifulSoup(response.text,"html.parser")
-chart = soup.find(class_='jsx-413197148 jsx-556393929 transition-ratio-stock__body')
+chart = soup.find(class_='要爬取的class名稱')
 print(chart)
 
-# find_all
-charts = soup.find_all(class_='jsx-413197148 jsx-556393929 transition-ratio-stock__body')
+# 如果有多個的話 使用 find_
+charts = soup.find_all(class_='要爬取的class名稱')
 for chart in charts:
   print(chart.text)
