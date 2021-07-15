32ed62a52b6461ea72589096a50a60c372ab245c
cjhsu1991 <dd86042@gmail.com>
2021-07-15 20:41:41 +0800

    Update selenium.py

--- selenium.py
+++ selenium.py
@@ -8,11 +8,10 @@ chrome_options.add_argument("--headless")
 chrome = webdriver.Chrome(chrome_options=chrome_options) # 如果 driver 沒跟你程式放一起 可以這樣寫driver = webdriver.Chrome("這裡放你的driver路徑")
 
 chrome.get("輸入要抓取的網址") # 輸入要抓取的網址
-text_data = chrome.find_element_by_class_name("要抓取的class的名稱").text
-print("開盤價:",price_text)
+text_data = chrome.find_element_by_class_name("要抓取的class的名稱").text # 取得特定class的資料(remove html tag後)
 
 text_data = chrome.find_element_by_id_name("要抓取的id的名稱").text # 取得特定id的資料(remove html tag後)
 
-# 取得特定 id 資料的 html source code
-element = chrome.find_element_by_class_name("要抓取的class的名稱")
-print(element.get_attribute("innerHTML"))
+
+element = chrome.find_element_by_class_name("要抓取的class的名稱") 
+print(element.get_attribute("innerHTML")) # 取得特定 id 資料的 html source code
