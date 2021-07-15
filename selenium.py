ab73331d6228c26d4df887e3a8ade3dc628a01ac
cjhsu1991 <dd86042@gmail.com>
2021-07-15 20:25:55 +0800

    Update selenium.py

--- selenium.py
+++ selenium.py
@@ -7,17 +7,12 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome = webdriver.Chrome(chrome_options=chrome_options) # 如果 driver 沒跟你程式放一起 可以這樣寫driver = webdriver.Chrome("這裡放你的driver路徑")
 
-chrome.get("https://finance.sina.com.cn/futures/quotes/I0.shtml") # 輸入要抓取的網址
-price_text = chrome.find_element_by_class_name("open-price").text
+chrome.get("輸入要抓取的網址") # 輸入要抓取的網址
+text_data = chrome.find_element_by_class_name("要抓取的class名稱").text
 print("開盤價:",price_text)
 
-red_text = chrome.find_element_by_class_name("price").text
-print("現價:",red_text)
+text_data = chrome.find_element_by_id_name("要抓取的id名稱").text # 取得特定id的資料(remove html tag後)
 
-# 漲幅 amt
-amt_text = chrome.find_element_by_class_name("amt").text
-print("漲幅:",amt_text)
-
-# 成交量 volume
-volume_text = chrome.find_element_by_class_name("volume").text
-print("成交量:",volume_text)
+# 取得特定 id 資料的 html source code
+element = chrome.find_element_by_id_name("要抓取的id名稱")
+print(element.get_attribute('innerHTML'))
