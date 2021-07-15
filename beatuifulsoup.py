64eab0a9826f13acee2a828d7ec34ac440049d32
cjhsu1991 <dd86042@gmail.com>
2021-07-15 20:44:20 +0800

    Update beatuifulsoup.py

--- beatuifulsoup.py
+++ beatuifulsoup.py
@@ -16,4 +16,15 @@ print(chart)
 # 如果有多個的話 使用 find_
 charts = soup.find_all(class_='要爬取的class名稱')
 for chart in charts:
-  print(chart.text)
+    print(chart.text)
+
+  
+# 取得 tag 為 <title></title>的資料
+title = soup.find("title")
+# 印出 title 的文字 remove html tag 後
+print(title.text)
+
+# 取得 <h3> tag 的所有資料
+h3s = soup.find_all("h3")
+for h3 in h3s:
+    print(h3.text)
